d8f57e555fb4d9dabb20d46e8f39b5c5d22d54e7
lcarter056 <94881226+lcarter056@users.noreply.github.com>
2025-04-11 16:50:45 -0500

    add DropDown and ActivityPage content

--- api/weather.js
+++ api/weather.js
@@ -1,17 +1,4 @@
 
-//temporary san antonio coordinates 
-/*
-const lat = 29.42;
-const long = -98.49;
-const startDate = "2025-04-04";
-const endDate = "2025-04-04";
-const timeZone = "America/Denver";
-*/
-
-
-
-
-
 async function fetchWeather(lat, long, startDate, endDate, timeZone) {
     const url = `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${long}&hourly=temperature_2m&temperature_unit=fahrenheit&start_date=${startDate}&end_date=${endDate}&timezone=${timeZone}`;
 
@@ -30,7 +17,7 @@ async function fetchWeather(lat, long, startDate, endDate, timeZone) {
       var weather = [];
       // unharcode 24
       for (let i=0; i < 24; i++){
-        weather.push({"Time": temptimes[i].split('T')[1], "Temp": temps[i]});
+        weather.push({"time": temptimes[i].split('T')[1], "temp": temps[i]});
       }
     
       return weather;
